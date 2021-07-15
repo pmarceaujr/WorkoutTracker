6e5491765dfcf3b190eb2aea33bac03c2c5cb73f
Paul Marceau <paul@themarceaus.net>
2021-07-14 23:52:51 -0400

    Added html and api route code to the routes files.

--- controllers/routes/api/routes.js
+++ controllers/routes/api/routes.js
@@ -2,7 +2,40 @@ console.log("in API post routes")
 const router = require('express').Router();
 const db = require("../../../models");
 
-
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+})
+// router.get to pull up info for the range page
+router.get("/api/workouts/range", ({ }, res) => {
+    db.Workout.find({}).then((dbWorkout) => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+// router.post to submit new completed workouts
+router.post("/api/workouts/", (req, res) => {
+    db.Workout.create(req.body).then((dbWorkout) => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+// router.put to update workouts by MongoDB _id value and update the exercsise body
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        { _id: req.params.id }, { exercises: req.body }
+    ).then((dbWorkout) => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 
 // Export the api routes
--- controllers/routes/html/routes.js
+++ controllers/routes/html/routes.js
@@ -1,7 +1,32 @@
 console.log("in HTML post routes")
+const path = require("path");
+console.log(__dirname)
+let path1 = path.join(__dirname, "../../../public/index.html")
+console.log(path1)
 const router = require('express').Router();
 
-//const { Posts, Comments, Users, Lookups } = require('../../models');
+// Add the GET request for the home page (index.html)
+router.get("/", async (req, res) => {
+    let path1 = path.join(__dirname, "../../../public/index.html")
+    console.log(path1)
+    res.sendFile(path.join(__dirname, "../../../public/index.html"));
+});
+
+// Add the GET request for the exercise page
+router.get("/exercise", async (req, res) => {
+    let path1 = path.join(__dirname, "../../../public/exercise.html")
+    console.log(path1)
+    res.sendFile(path.join(__dirname, "../../../public/exercise.html"));
+});
+
+// Add GET request for the Stats Page
+router.get("/stats", async (req, res) => {
+    let path1 = path.join(__dirname, "../public/stats.html")
+    console.log(path1)
+    res.sendFile(path.join(__dirname, "../../../public/stats.html"));
+});
+
+
 
 // Export the HTML routes
 module.exports = router;
