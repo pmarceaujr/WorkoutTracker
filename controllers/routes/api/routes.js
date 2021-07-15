699fbecd87099c0776cdd8cde988d7b470171bae
Paul Marceau <paul@themarceaus.net>
2021-07-14 22:52:17 -0400

    Created initial layout for API and HTML routes

---
+++ controllers/routes/api/routes.js
@@ -0,0 +1,9 @@
+console.log("in API post routes")
+const router = require('express').Router();
+const db = require("../../../models");
+
+
+
+
+// Export the api routes
+module.exports = router;
---
+++ controllers/routes/html/routes.js
@@ -0,0 +1,7 @@
+console.log("in HTML post routes")
+const router = require('express').Router();
+
+//const { Posts, Comments, Users, Lookups } = require('../../models');
+
+// Export the HTML routes
+module.exports = router;
