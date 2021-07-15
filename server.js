7327e4df288b9699308d4459f4b445b36abed542
Paul Marceau <paul@themarceaus.net>
2021-07-14 23:52:24 -0400

    Changed route file paths

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require('path');
 //! require("./seeders/seed"); // Either npm run seed or keep this in here uncommented and it will run the seed
 
 const PORT = process.env.PORT || 3025;
@@ -11,7 +12,7 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
