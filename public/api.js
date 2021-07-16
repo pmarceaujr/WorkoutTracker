775bc8e50a83fbbc4cb144b1011397d54c554c48
Paul Marceau <paul@themarceaus.net>
2021-07-16 00:09:15 -0400

    Adjusted spacing

--- public/api.js
+++ public/api.js
@@ -10,6 +10,7 @@ const API = {
 
     return json[json.length - 1];
   },
+
   async addExercise(data) {
     const id = location.search.split("=")[1];
 
@@ -23,6 +24,7 @@ const API = {
 
     return json;
   },
+
   async createWorkout(data = {}) {
     const res = await fetch("/api/workouts", {
       method: "POST",
@@ -42,3 +44,4 @@ const API = {
     return json;
   },
 };
+
