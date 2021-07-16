14068bf9a51d5a9b0ec604688cc66edc9a64768f
Paul Marceau <paul@themarceaus.net>
2021-07-16 00:09:01 -0400

    Fixed adding exercises error

--- controllers/routes/api/routes.js
+++ controllers/routes/api/routes.js
@@ -1,41 +1,77 @@
 console.log("in API post routes")
 const router = require('express').Router();
-const db = require("../../../models");
+const Workout = require("../../../models/workout");
 
-router.get("/api/workouts", (req, res) => {
-    db.Workout.find({}).then(dbWorkout => {
-        res.json(dbWorkout);
-    })
+//Get the last workout
+router.get("/api/workouts", async (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ])
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+
         .catch(err => {
             res.status(400).json(err);
         });
-})
-// router.get to pull up info for the range page
-router.get("/api/workouts/range", ({ }, res) => {
-    db.Workout.find({}).then((dbWorkout) => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
 });
-// router.post to submit new completed workouts
+
+
+
+// Add a new exercise
+router.put("/api/workouts/:id", async (req, res) => {
+    // try {
+    Workout.findByIdAndUpdate(req.params.id,
+        { $push: { exercises: req.body } },
+        { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        // }
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+// Add a new Workout
 router.post("/api/workouts/", (req, res) => {
-    db.Workout.create(req.body).then((dbWorkout) => {
+    Workout.create(req.body).then((dbWorkout) => {
         res.json(dbWorkout);
     }).catch(err => {
         res.status(400).json(err);
     });
 });
-// router.put to update workouts by MongoDB _id value and update the exercsise body
-router.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(
-        { _id: req.params.id }, { exercises: req.body }
-    ).then((dbWorkout) => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
-});
+
+// Get the workouts in a range
+router.get("/api/workouts/range", ({ }, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ])
+        .sort({
+            _id: -1
+        })
+        .limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        });
+})
+
+
 
 
 // Export the api routes
--- controllers/routes/html/routes.js
+++ controllers/routes/html/routes.js
@@ -1,28 +1,25 @@
 console.log("in HTML post routes")
 const path = require("path");
 console.log(__dirname)
-let path1 = path.join(__dirname, "../../../public/index.html")
-console.log(path1)
+console.log(path.join(__dirname, "../../../public/index.html"))
+
 const router = require('express').Router();
 
 // Add the GET request for the home page (index.html)
 router.get("/", async (req, res) => {
-    let path1 = path.join(__dirname, "../../../public/index.html")
-    console.log(path1)
+    console.log(path.join(__dirname, "../../../public/index.html"))
     res.sendFile(path.join(__dirname, "../../../public/index.html"));
 });
 
 // Add the GET request for the exercise page
 router.get("/exercise", async (req, res) => {
-    let path1 = path.join(__dirname, "../../../public/exercise.html")
-    console.log(path1)
+    console.log(path.join(__dirname, "../../../public/exercise.html"))
     res.sendFile(path.join(__dirname, "../../../public/exercise.html"));
 });
 
 // Add GET request for the Stats Page
 router.get("/stats", async (req, res) => {
-    let path1 = path.join(__dirname, "../public/stats.html")
-    console.log(path1)
+    console.log(path.join(__dirname, "../public/stats.html"))
     res.sendFile(path.join(__dirname, "../../../public/stats.html"));
 });
 
